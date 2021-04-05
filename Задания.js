6b38f726330d58fd4865d41e65e155effbddd91b
Alina-GR <81136228+Alina-GR@users.noreply.github.com>
2021-04-05 22:07:24 +0300

    Add files via upload

--- Задания.js
+++ Задания.js
@@ -1,36 +1,40 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-movies: {
-    'logan: 87'
-};
-if(numberOfFilms)
+function  sumInput(){
+console.Напишите ("Введите количество прошедших месяцев:");    
+m = console.ReadLine();
+n = m % 12;
+строка = "";
+переключатель(п)
 {
-    console.log(numberOfFilms)
-};
-const personalMovieDB = {
-    const: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-if(personalMovieDB)
-{
-    console.log(personalMovieDB);
-};
-const a = prompt("Один из последних просмотренных фильмов?")
-movies: {
-    'logan: Сумерки'
-};
-b = prompt("На сколько оцените его?"), 
-c = prompt("Один из последних просмотренных фильмов?"),
-d = prompt("На сколько оцените его?");
-movies: {
-    'logan: 10 баллов'
-};
+    case1: console.WriteLine("Январь"); перерыв;
+    case2: console.WriteLine("Февраль"); перерыв;
+    case3: console.WriteLine("Март"); перерыв;
+    case4: console.WriteLine("Апрель"); перерыв;
+    case5: console.WriteLine("Май"); перерыв;
+    case6: console.WriteLine("Июнь"); перерыв;
+    case7: console.WriteLine("Июль"); перерыв;
+    case8: console.WriteLine("Август"); перерыв;
+    case9: console.WriteLine("Сентябрь"); перерыв;
+    case10: console.WriteLine("Октябрь"); перерыв;
+    case11: console.WriteLine("Ноябрь"); перерыв;
+    case12: console.WriteLine("Декабрь"); перерыв;
+}
+console.WriteLine("Это был месяц:" + строка);
+console.ReadLine();
+}
 
-personalMovieDB.movies.a = b;
-personalMovieDB.movies.c = d;
-if(personalMovieDB)
+
+сonsole.Write("Размерность массива: ");
+int n = Convert.ToInt32(сonsole.ReadLine());  
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
 {
-    console.log(personalMovieDB);
-};
+    сonsole.Write("array[{0}] = ",i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int min = 0;
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] < array[i - 1]) min = i; 
+}
+Console.WriteLine("Минимальное значение массива: " + array[min]);
+Console.ReadLine();
